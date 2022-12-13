c221805378bc1a342d6055e11dede79d8b183394
wveimar <wveimar.mamian@gmail.com>
2022-12-13 08:42:40 -0500

    maestros ceramistas cards 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/common/Layout";
 import PageMaestros from "./pages/maestros/PageMaestros";
+import PageNoticias from "./pages/noticias/PageNoticias";
 import Page from "./pages/page/Page";
 import PostDetail from "./pages/post-detail/PostDetail";
 import { PAGE_CODES } from "./utils/page-codes";
@@ -16,6 +17,10 @@ function App() {
         <Route
           path={`/${PAGE_CODES.MAESTROS}`}
           element={<PageMaestros pageCode={PAGE_CODES.MAESTROS} />}
+        />
+         <Route
+          path={`/${PAGE_CODES.NOTICIAS}`}
+          element={<PageNoticias pageCode={PAGE_CODES.NOTICIAS} />}
         />
       </Routes>
     </Layout>
---
+++ src/pages/noticias/PageNoticias.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import Image from "../../components/image/Image";
+import PageSection from "../../components/page-section/PageSection";
+import GridCards from "../../components/grid-cards/GridCards";
+
+const PageNoticias = ({ pageCode }) => {
+  const { error, data } = useQuery(PAGE_NOTICIAS_QUERY, {
+    variables: {
+      where: { code_contains: pageCode },
+    },
+  });
+
+  if (error && error.networkError) {
+    return <p>Error: {error.networkError.result.errors[0].message}</p>;
+  }
+  if (!data) {
+    return <p>No Data!</p>;
+  }
+  const [image] = data.simplePageCollection.items;
+  console.log(data);
+
+  return (
+    <div>
+      <Image description={image.description} url={image.image.url} />
+      <PageSection sectionCode="s1" code={pageCode}>
+        <GridCards variant="card-link" code={`${pageCode}-s1`} />
+      </PageSection>
+    </div>
+  );
+};
+
+export default PageNoticias;
+
+const PAGE_NOTICIAS_QUERY = gql`
+  query simplePageCollection($where: SimplePageFilter) {
+    simplePageCollection(where: $where) {
+      items {
+        name
+        description
+        image {
+          url
+        }
+      }
+    }
+  }
+`;
