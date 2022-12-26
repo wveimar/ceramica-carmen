79b1d9821b8baba0ba7d214e7babf551d25b1f7e
wveimar <wveimar.mamian@gmail.com>
2022-12-26 07:37:57 -0500

    certification

---
+++ src/pages/certificacion/PageCertificacion.jsx
@@ -0,0 +1,55 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import Gallery from "../../components/gallery/Gallery";
+import GalleryFrontPage from "../../components/galleryFrontPage/GalleryFrontPage";
+import Loader from "../../components/loader/Loader";
+import PageSection from "../../components/page-section/PageSection";
+
+const PageGallery = ({ pageCode }) => {
+  const { error, data } = useQuery(PAGE_CERTIFICACION_QUERY, {
+    variables: {
+      where: { code_contains: pageCode },
+    },
+  });
+
+  if (error && error.networkError) {
+    return <p>Error: {error.networkError.result.errors[0].message}</p>;
+  }
+  if (!data) {
+    return <Loader />
+  }
+  const [page] = data.simplePageCollection.items;
+
+  console.log(page);
+
+  return (
+    <div style={{ marginTop: "4rem" }}>
+      <GalleryFrontPage DataImages={page.imagesCollection.items} />
+      {/* <PageSection sectionCode="s1" code={`${pageCode}`}>
+        <Gallery code={`certificacion-${pageCode}`} />
+      </PageSection> */}
+    </div>
+  );
+};
+
+export default PageGallery;
+
+const PAGE_CERTIFICACION_QUERY = gql`
+  query simplePageCollection($where: SimplePageFilter) {
+    simplePageCollection(where: $where) {
+      items {
+        name
+        description
+        image {
+          url
+        }
+        imagesCollection {
+          items {
+            url
+          }
+        }
+        hidenGallery
+      }
+    }
+  }
+`;
