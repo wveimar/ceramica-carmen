c32d517cf0d6dbc303f8ba0b292f5238a49d6850
wveimar <wveimar.mamian@gmail.com>
2023-01-30 17:57:15 -0500

    correciones 1

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,11 @@ import { PAGE_CODES } from "./utils/page-codes";
 
 function App() {
   return (
+
     <Layout>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@1&display=swap" rel="stylesheet"/>
       <Routes>
         <Route path="/" exact element={<Page pageCode="home" />} />
         <Route path="/page" exact element={<Page />} />
